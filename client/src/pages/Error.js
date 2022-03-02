622aa30c54239b9f569d6dc8c15ae9bbe21e7821
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-03-02 06:51:21 +0500

    Error Page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from "react";
+import img from "../assets/images/not-found.svg";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not-found" />
+        <h3>Oh! Not Found</h3>
+        <Link to="/">Go Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
