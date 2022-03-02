7b4652ef6c5bcd93cf3f6fcd79ff16b95ffd6a58
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-03-02 14:30:21 +0500

    Register Page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,68 @@
-import React from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useState } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMemeber: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const { name, email, password, isMemeber } = values;
+
+  const handleChange = (e) => console.log(e.target);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMemeber: !isMemeber });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>{isMemeber ? "Login" : "Register"}</h3>
+        {/* name Input */}
+        {!isMemeber && (
+          <FormRow
+            type="text"
+            name="name"
+            handleChange={handleChange}
+            value={name}
+          />
+        )}
+        {/* email Input */}
+        <FormRow
+          type="email"
+          name="email"
+          handleChange={handleChange}
+          value={email}
+        />
+        {/* password Input */}
+        <FormRow
+          type="password"
+          name="password"
+          handleChange={handleChange}
+          value={password}
+        />
+        <button className="btn btn-block">Submit</button>
+        <p>
+          {isMemeber ? "not a member?" : "already a member?"}
+
+          <button className="member-btn" onClick={toggleMember}>
+            {isMemeber ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
