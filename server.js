3b8652eb1bc599b695dd54d2cb7ca32c7e7de0c1
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-03-03 17:43:41 +0500

    Created Basic Server with ES6 Module

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Server is Running");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome Express");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on localhost:${PORT}`);
+});
