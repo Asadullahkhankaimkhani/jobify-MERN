c3f179ff324fede06f9734b3c423c9af49cecfa2
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-03-08 01:37:28 +0500

    Json web token setup and implementation

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,8 @@ export const register = async (req, res) => {
   }
 
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user, token });
 };
 
 export const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = mongoose.Schema({
   name: {
@@ -36,5 +37,11 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model("User", userSchema);
