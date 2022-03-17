c4f2715c49a5e17cec4f89395a79c997dad96dbe
Asadullah Khan <asadullahk15@gmail.com>
2022-03-17 10:52:03 +0500

    Dashboard Pages -Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { Landing, Error, Register } from "./pages";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<h1>Dashboard</h1>} />
       <Route path="/register" element={<Register />} />
       <Route path="/landing" element={<Landing />} />
       <Route path="*" element={<Error />} />
